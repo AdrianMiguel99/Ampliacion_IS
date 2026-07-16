ac98156cc05dbc2e8dcb4a917108675f36af9c96
adrianmiguel99 <adrian99xd@gmail.com>
2026-07-16 17:42:36 -0600

    Se agrega base del codigo

---
+++ FreezerApp/FreezerApp/Controllers/FreezerController.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreezerApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FreezerController : ControllerBase
+    {
+        private static bool _poweredOn = true;
+        private static double _currentTemperature = 5;
+        private static double _targetTemperature = -18;
+
+
+        [HttpPost("target-temperature")]
+        public IActionResult SetTargetTemperature(double temperature)
+        {
+            if (temperature > 0)
+            {
+                return BadRequest("Target temperature must be below 0");
+            }
+
+            _targetTemperature = temperature;
+
+            return Ok("Target temperature updated");
+        }
+
+        [HttpPost("cool")]
+        public IActionResult CoolDown()
+        {
+            if (!_poweredOn)
+            {
+                return BadRequest("Freezer is off");
+            }
+
+            int resourcesUsed = CalculateResources();
+
+            _currentTemperature -= resourcesUsed * 0.05;
+
+            if (_currentTemperature < _targetTemperature)
+            {
+                _currentTemperature = _targetTemperature;
+            }
+
+            return Ok(new
+            {
+                Message = "Cooling completed",
+                CurrentTemperature = _currentTemperature
+            });
+        }
+
+        private int CalculateResources()
+        {
+            double difference =
+                _currentTemperature - _targetTemperature;
+
+            //30 is the the difference between the current temperature and the target temperature, if the difference is greater than 30, the qty of resources used is 150
+            // those are grades 
+            if (difference > 30)
+            {
+                //Qty of resources used is 150 if the difference is greater than 30
+                return 150;
+            }
+
+            //20 is the the difference between the current temperature and the target temperature, if the difference is greater than 20, the qty of resources used is 120
+            // those are grades
+            if (difference > 20)
+            {
+                //Qty of resources used is 120 if the difference is greater than 20
+                return 120;
+            }
+
+            //10 is the the difference between the current temperature and the target temperature, if the difference is greater than 10, the qty of resources used is 100
+            // those are grades
+            if (difference > 10)
+            {
+                //Qty of resources used is 100 if the difference is greater than 10
+                return 100;
+            }
+
+            return 50;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# frezeerApp
+
+This app is using .Net 10 including a FreezerApp with the API project and FreezerApiTests for the unit test project using NUnit
