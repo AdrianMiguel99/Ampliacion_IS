84f441ebb6126249da60293dc3dc2c9f05caae7b
adrianmiguel99 <adrian99xd@gmail.com>
2026-07-16 19:51:51 -0600

    Se agregan 2 unitest para probar el seteo correcto y erroneo de una temperatura

---
+++ FreezerApp/FreezerApiTests/FreezerServiceTest.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace FreezerApp.FreezerApiTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {   
+            setTargetTemperature_InvalidTemperature_ShouldThrowException();
+            setTargetTemperature_ValidTemperature_ShouldSetTemperature();
+        }
+
+        [Test]
+        public void setTargetTemperature_ValidTemperature_ShouldSetTemperature()
+        {
+            // Arrange
+            var freezerService = new FreezerApi.Service.FreezerService();
+
+            // Act
+            freezerService.SetTargetTemperature(-5.0);
+
+            // Assert
+            Assert.AreEqual(-5.0, freezerService.GetTargetTemperature());
+
+        }
+
+        [Test]
+        public void setTargetTemperature_InvalidTemperature_ShouldThrowException()
+        {
+            // Arrange
+            var freezerService = new FreezerApi.Service.FreezerService();
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => freezerService.SetTargetTemperature(10.0));
+        }
+    }
+}
